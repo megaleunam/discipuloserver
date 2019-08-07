4cea30e58ea12430226c66b24bef6551f78ca62f
Manuel <manuel.marino03@gmail.com>
2019-08-07 09:23:46 -0400

    cambios

--- src/discipulos/discipulos.module.ts
+++ src/discipulos/discipulos.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { DiscipuloSchema } from './schemas/discipulo.schema';
 import { DiscipulosService } from './discipulos.service';
 import { DiscipulosController } from './discipulos.controller';
-import { PersonaSchema } from 'src/personas/schemas/personas.schema';
+import { PersonaSchema } from '../personas/schemas/personas.schema';
 
 @Module({
     imports: [
